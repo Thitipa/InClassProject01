0a4c1e0a179dfcffc66cd10dc912c9ca2f9da3f8
Thitipa <thitipanet@gmail.com>
2022-05-20 15:25:12 +0700

    Link the nav

--- src/components/TipLogin.js
+++ src/components/TipLogin.js
@@ -1,50 +1,68 @@
 import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { Box, Button } from "@mui/material";
-import Dis from "../images/discord_logo.png"
-import { Nav, NavItem } from 'reactstrap';
-import { NavLink, Switch } from 'react-router-dom';
+import Dis from "../images/discord_logo.png";
+import { Nav, NavItem } from "reactstrap";
+import { NavLink as RRNavLink } from "react-router-dom";
+import { NavLink } from "reactstrap";
 
 function TipLogin() {
   return (
     <div>
       <Nav
         className="d-flex justify-content-between navbar navbar-light bg-light"
-        style={{ position:"fixed", width:"100%" , paddingLeft:"50px" , paddingRight:"50px", bgcolor: "white", color: "black" }}
+        style={{
+          position: "fixed",
+          width: "100%",
+          paddingLeft: "50px",
+          paddingRight: "50px",
+          bgcolor: "white",
+          color: "black",
+        }}
       >
-        <div> <img src={Dis} alt="discord" width={50}></img>Due Today, Do Tomorrow</div>
-        <Nav pills><NavItem
-          style={{
-            color: "black",
-            backgroundColor: "#E5E5E5",
-            border: "none",
-            width: "120px",
-            height: "40px",
-            textAlign:"center",
-            textJustify:"center",
-            borderRadius: "10px",
-            marginRight: "20px"
-          }}
-        >
-          Login
-        </NavItem>
-        <NavItem
-          style={{
-            color: "black",
-            backgroundColor: "#E5E5E5",
-            border: "none",
-            width: "120px",
-            height: "40px",
-            textAlign:"center",
-            textJustify:"center",
-            borderRadius: "10px",
-          }}
-          
-        >
-          Home
-        </NavItem>
+        <div>
+          <img src={Dis} alt="discord" width={50}></img>Due Today, Do Tomorrow
+        </div>
+        <Nav pills>
+          <NavItem>
+            <NavLink
+              style={{
+                color: "black",
+                backgroundColor: "#E5E5E5",
+                width: "120px",
+                padding:"3px",
+                textAlign: "center",
+                textJustify: "center",
+                borderRadius: "10px",
+                marginRight: "20px",
+              }}
+              tag={RRNavLink}
+              exact
+              to="/login"
+            >
+              Login
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              style={{
+                color: "black",
+                backgroundColor: "#E5E5E5",
+                width: "120px",
+                padding:"3px",
+                textAlign: "center",
+                textJustify: "center",
+                borderRadius: "10px",
+                marginRight: "20px",
+              }}
+              tag={RRNavLink}
+              exact
+              to="/home"
+            >
+              Home
+            </NavLink>
+          </NavItem>
         </Nav>
-        
       </Nav>
     </div>
   );
